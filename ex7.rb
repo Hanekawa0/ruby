ee825fa08d582f274ba372d43b4287b22272553b
Hanekawa0 <shoukaku00@gmail.com>
2017-08-19 16:23:20 +0200

    yay more commits

--- ex7.rb
+++ ex7.rb
@@ -1,7 +1,22 @@
-A1 = 2
-A2 = 2
-A3 = 3
-A4 = 4
+puts "I have a little cat"
+puts "It is white as a #{'snow'}"
+puts "It is really cute"
 
 
-puts = "#{A1 + A2 + A3 + A4}"
+end0 = " "
+end1 = "G"
+end2 = "l"
+end3 = "a"
+end4 = "s"
+
+end5 = "o"
+end6 = "f"
+
+end7 = "w"
+end8 = "a"
+end9 = "t"
+end10 = "e"
+end11 = "r"
+
+print end1 + end2 + end3 + end4 * 2 + end0
+puts end5 + end6 + end0 + end7 + end8 + end9 + end10 + end11 
